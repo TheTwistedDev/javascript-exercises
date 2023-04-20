f20e24cb87dd63025e889ed6e837273c0f197dba
Twisted <TheTwistedDev@gmail.com>
2023-04-20 15:22:27 -0700

    completed 2nd problem

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, i) {
+    word = ""
+    if (i < 0) return "ERROR";
+    for (let j = 0; j < i; j++) {
+        word += str;
+    }
+    return word;
 };
 
 // Do not edit below this line
